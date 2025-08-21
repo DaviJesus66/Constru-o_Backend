780392af81bc2899c060474abd62ac510fb175b6
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-08-20 21:42:02 -0300

    nova atualização

---
+++ Práticas/01-primeiro-projeto/index.js
@@ -0,0 +1,21 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "Davi"
+
+let sobreNome = "Jesus"
+
+console.log(nome + " " + sobreNome)
+
+let idade = 35
+
+console.log("A idade do " + nome + " é " + idade)
+
+let exercícios = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let notaA1Calculada = exercícios + trabalho + prova
+
+console.log("Nota A1: " + notaA1Calculada)
