9964e5289c9ed19a7cbca7d2fd48c5e630b38dbc
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-09-11 01:58:24 -0300

    inicio

---
+++ Exercícios/Calculadora em API REST/index.js
@@ -0,0 +1,3 @@
+//
+const calculadoraRouter = require('./routes/calculadora');
+app.use('/calculadora', calculadoraRouter);
---
+++ Exercícios/Calculadora em API REST/routes/calculadora.js
@@ -0,0 +1,48 @@
+//criando endpoints para cada operação
+//SOMA
+router.get('/somar', (req, res) => {
+// Number() converte string para número
+const numA = Number(req.query.numA);
+const numB = Number(req.query.numB);
+// implementação da soma...
+});
+
+//SUBTRAÇÃO
+router.get('/subitração', (req, res) => {
+// Number() converte string para número
+const numA = Number(req.query.numA);
+const numB = Number(req.query.numB);
+// implementação da soma...
+});
+
+//MULTIPLICAÇÃO
+router.get('/multiplicação', (req, res) => {
+// Number() converte string para número
+const numA = Number(req.query.numA);
+const numB = Number(req.query.numB);
+// implementação da soma...
+});
+
+//DIVISÃO
+router.get('/divisão', (req, res) => {
+// Number() converte string para número
+const numA = Number(req.query.numA);
+const numB = Number(req.query.numB);
+// implementação da soma...
+});
+
+//AO QUADRADO
+router.get('/aoQuadrado', (req, res) => {
+// Number() converte string para número
+const numA = Number(req.query.numA);
+const numB = Number(req.query.numB);
+// implementação da soma...
+});
+
+//Raiz Quadrada
+router.get('/raizQuadrada', (req, res) => {
+// Number() converte string para número
+const numA = Number(req.query.numA);
+const numB = Number(req.query.numB);
+// implementação da soma...
+});
