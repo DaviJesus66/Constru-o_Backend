cf17b6d885160c4ee46d15276420e44f3172de6e
alysonviana <alyson.viana@iesb.edu.br>
2025-09-24 21:19:02 -0300

    criando o corpo do aluno

--- Exercícios/04CRUD-ESCOLA/routes/alunos.js
+++ Exercícios/04CRUD-ESCOLA/routes/alunos.js
@@ -1,1 +1,55 @@
-// texte pqp ap
+// Criação de aluno
+
+const express = require('express')
+const router = express.Router()
+
+let listeAlunos = [
+    {
+        id : 1,
+        nome: "Fernando",
+        cpf : "12345678901",
+        email : "fernando@aluno.com",
+        dataNacimento: "01/02/2000"
+    },
+    {
+        id : 2,
+        nome: "Gabriela",
+        cpf: "012345678902",
+        email: "gabriela@aluno.com",
+        dataNacimento: "07/05/2002"
+    },
+]
+
+router.get('/alunos', (req, res, nest)=>{
+    res.json(listeAlunos)
+})
+
+router.get('/alunos/:id', (req, res, nest,) =>{
+    const id = req.params.id
+    const alunos = listeAlunos.find(alunos = alunos.id == id)
+    if(!alunos){
+        return res.status(404).json({error: "Aluno nao encontrado!!"})
+    }
+    res.json(alunos)
+})
+
+router.post('/alunos', (req, res, nest) =>{
+    const {nome, cpf, email, dataNacimento} = req.body
+    if(!nome|| !cpf|| !email|| !dataNacimento){
+        return res.status(400).json({error: "Nome , CPF, Email E DataNacimento SÃO OBRIGATORIOS!!!"})
+    }
+    if(listeAlunos.some(alunos => alunos.cpf == cpf)){
+        return res.status(409).json({error: "Este CPF Ja Esta Cadastrado!!!"})
+    }
+
+    const novoAluno = {
+        id: Date.new(),
+        nome,
+        cpf,
+        email,
+        dataNacimento
+    }
+    listeAlunos.push(novoAluno)
+    res.status(201).json({
+        massage: "Aluno cadastrada com sucesso", novoAluno})
+})
