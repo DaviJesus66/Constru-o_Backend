e1d5a722f4c7e3e76714a4c2f9633e1879f18e60
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-10-08 22:58:08 -0300

    atul

--- Práticas/07-crud-tarefas/index.js
+++ Práticas/07-crud-tarefas/index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 
 // conectar no banco mongo//
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect('mongodb+srv://Davvisj:43sJJnGw3Ub7UqPs@cluster0.hgjgzrq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => {
