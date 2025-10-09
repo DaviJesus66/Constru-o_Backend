e98747a7ecad40cf586f5477707d35c34e39b96a
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-10-08 22:19:46 -0300

    nova aula

---
+++ Práticas/07-crud-tarefas/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+
+app.use(express.json());
+
+// conectar no banco mongo
+
+mongoose.connect('mongodb+srv://Davvisj:<db_password>@cluster0.hgjgzrq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        console.log('Conectado ao MongoDB')
+    })
+    .catch((error) => {
+        console.error('Erro ao conectar ao MongoDB:', error)
+    })
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000')
+})
