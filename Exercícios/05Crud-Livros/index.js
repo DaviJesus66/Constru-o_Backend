f39e022c3b214afb0592ee4665eee10d640a8571
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-10-16 23:49:34 -0300

    att

--- Exercícios/05Crud-Livros/index.js
+++ Exercícios/05Crud-Livros/index.js
@@ -46,14 +46,32 @@ app.post('/livros', async (req, res, next) => {
         return res.status(400).json({ error: 'Nome é obrigatório' });
     }
     const livroCriado = await livrosModel.create(livro)
-    res.status(201).json(livroCriada)
+    res.status(201).json(livroCriado)
 })
 
 //read
 app.get('/livros', async (req, res, next) => {
-    const livros = await livroModel.find()
-    res.json(livros)
-})
+  try {
+    const livros = await livroModel.find();
+    res.status(200).json(livros);
+  } catch (erro) {
+    res.status(500).json({ message: 'Erro ao buscar livros', erro });
+  }
+});
+
+
+//Buscar
+app.get('/livros/:id', async (req, res) => {
+  try {
+    const livro = await Livro.findById(req.params.id);
+    if (!livro) {
+      return res.status(404).json({ message: 'Livro não encontrado' });
+    }
+    res.status(200).json(livro);
+  } catch (erro) {
+    res.status(400).json({ message: 'Erro ao buscar livro', erro });
+  }
+});
 
 //update
 app.put('/livros/:id', async (req, res, next) => {
