8a171b8ad346a5d9bcdb094559dfc562876a444d
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-10-22 20:47:59 -0300

    WFE

--- Práticas/08-crud-pessoas/src/index.js
+++ Práticas/08-crud-pessoas/src/index.js
@@ -1,30 +1,31 @@
-const express = require ('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const express = require('express')
+const app = express()
 
-const app = express();
+app.use(express.json())
 
-app.use(express.json());
+// conexão com o banco de dados
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
-// conectar no banco mongo//
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-const DB_NAME = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_NAME = process.env.DB_NAME
 
-const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.connect(url)
-    .then(() => {
-        console.log('Conectado ao MongoDB')
-    })
-    .catch((error) => {
-        console.error('Erro ao conectar ao MongoDB:', error)
-    })
+  .then(() => {
+    console.log("Conectado ao MongoDB")
+  })
+  .catch(err => {
+    console.log("Erro ao conectar no banco MongoDB: ", err)
+  })
 
+// controladores e rotas
+const PessoaController = require('./controllers/PessoaController')
+app.use(PessoaController)
 
-
-//start
 app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000')
+  console.log("Aplicação rodando em http://localhost:3000")
 })
